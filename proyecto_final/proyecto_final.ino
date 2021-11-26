be530df8145d39564f29e6bc60e15e5321ff6e6e
lfer9980 <luis_fernandez99@hotmail.com>
2021-11-25 21:00:30 -0700

    añadidos comentarios y librerias por si acaso

--- proyecto_final/proyecto_final.ino
+++ proyecto_final/proyecto_final.ino
@@ -4,31 +4,44 @@
 #include <LiquidCrystal_I2C.h>
 #include <Wire.h>
 
+
+//instanciando LCD
 LiquidCrystal_I2C lcd(0x3f, 16, 2);
+
+//instanciando DHT
+DHT dht(DHTPin, DHTTYPE);
 
 
 /* variables del sistema */
 //DHT
 #define DHTTYPE DHT22
 
+
 /* variables para los sensores */
 const int DHTPin = 4;  
+
+/* variables para el sensor ultrasonico */
 const int ECO = 2;
 const int TRIG = 3;
-const int OUT = 8;
 int DURATION;
 int DISTANCE;
-int i = 0;
+
+/* pin de salida al humidificador  */
+const int OUT = 8;
+
+/* variables del sensor dht11 */
 float hum;
 float temp;
 
-//instanciando DHT
-DHT dht(DHTPin, DHTTYPE);
+//VARIABLE ITERADORA
+int i = 0;
 
 
 void setup() {
 	dht.begin();
 	Serial.begin(9600);
+
+	/* definiendo los pines del ultrasonico y salida */
 	pinMode(TRIG, OUTPUT);
 	pinMode(ECO, INPUT);
 	pinMode(OUT, OUTPUT);
@@ -45,6 +58,7 @@ void setup() {
 
 
 void loop() {
+	/* imprime en que ciclo estoy */
 	Serial.println("ciclo: ");
 	Serial.println(i);
 
@@ -63,6 +77,7 @@ void loop() {
 		Serial.println(DISTANCE);
 		delay(200);
 
+		/* evaluo si hay alguien entre 40 y 2 */
 		if (DISTANCE <= 40 && DISTANCE >= 2) {
 			hum = dht.readHumidity();
 			temp = dht.readTemperature();
@@ -72,6 +87,7 @@ void loop() {
 			Serial.println("temperatura: ");
 			Serial.println(temp);
 
+			/* if de proteccion por si no lee bien el dht */
 			if (isnan(hum) || isnan(temp)) {
 				lcd.clear();
 				lcd.setCursor(0, 0);
@@ -101,6 +117,7 @@ void loop() {
 			lcd.setCursor(6,1);
 			lcd.print(temp);
 			delay(100);
+			/* encendido del humidificador */
 			digitalWrite(OUT, HIGH);
 			Serial.println("ON");
 			delay(10000);
@@ -120,7 +137,7 @@ void loop() {
 		Serial.println("temperatura: ");
 		Serial.println(temp);
 
-		
+		/* la temperatura es mayor a 40? */
 		if (hum <= 40) {
 			lcd.clear();
 			lcd.setCursor(0, 0);
@@ -152,8 +169,10 @@ void loop() {
 		i++;
 	}
 
+	/* apago el humidificador */
 	digitalWrite(OUT, LOW);
 
+	/* validacion para volver a comenzar */
 	if (i == 4) {
 		i = 0;
 	}
